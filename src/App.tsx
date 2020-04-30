1f646b040a0e66700f0fae9f9f39de7d901be19d
dasdware <info@dasdware.de>
2020-04-30 18:46:46 +0200

    Remove debug output

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ const App: FunctionalComponent = () => {
 
     const result = (!controller.northFormula.haveError && !controller.eastFormula.haveError)
         ? <div class="result">
-            <div>{controller.northFormula.result} - {controller.northFormula.decimalResult}</div>
-            <div>{controller.eastFormula.result} - {controller.eastFormula.decimalResult}</div>
+            <div>{controller.northFormula.result}</div>
+            <div>{controller.eastFormula.result}</div>
           </div>
         : <div class="error">
             At least one of the formulas has an error or cannot be calculated.
@@ -22,9 +22,6 @@ const App: FunctionalComponent = () => {
             <div class="sidebar" style="z-index: 9999">
                 <div class="content">
                     <h1>Geocaching Formula Calculator</h1>
-
-                    {/* <div>N 51 ° 0(C-L+1).([G/6-F]+2)(I-H+D)(F-I-4)</div>
-                    <div>E 013° L(A+2).(K-F)(G/K-E+D)(A+E)</div> */}
 
                     <h2>North coordinate</h2>
                     <FormulaInput formula={controller.northFormula} />
